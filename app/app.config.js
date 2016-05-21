609817011d20311bd3827957b08a8ca52468f1c2
rightlag <rightlag@gmail.com>
2016-05-21 16:05:30 +0000

    Adding core files+directory structure

---
+++ app/app.config.js
@@ -0,0 +1,9 @@
+(function () {
+  'use strict';
+  angular
+    .module('urban')
+    .config(config);
+  function config() {
+    // Configuration for 'urban' application.
+  }
+}());
---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+(function () {
+  'use strict';
+  var gulp = require('gulp');
+  var browserSync = require('browser-sync');
+  gulp.task('serve', function() {
+    browserSync({
+      server: {
+        baseDir: './app',
+        routes: {
+          '/bower_components': './bower_components'
+        }
+      }
+    });
+  });
+}());
